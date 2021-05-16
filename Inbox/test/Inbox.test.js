bf15f776e5d33ccbb00e696324ff92410b4c207b
llc <touldeguiacranmer@gmail.com>
2021-05-16 06:40:03 +0000

    Added useful test for project.

--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -3,20 +3,14 @@ const ganache = require('ganache-cli');
 // The constructor function for web3
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-
-class Car {
-	park() {
-		return 'stopped';
-	}
-	
-	drive() {
-		return 'zoom zoom';
-	}
-}
-
-describe('Car', () => {
-	it('can park', () => {
-		const c = new Car();
-		assert.equal(c.park(), 'stopped');
-	});
+let accounts;
+beforeEach(async () => {
+	// return list of all accounts
+	accounts = await web3.eth.getAccounts();
+	// use an account to deploy contract
+	console.log(accounts)
 });
+
+describe('Inbox', () => {
+	it('deploys a contract', () => {});
+})
