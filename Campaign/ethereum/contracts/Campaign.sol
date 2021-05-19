4b968ebeb243a7e89e589a237f7a0bd2beadb47d
llc <touldeguiacranmer@gmail.com>
2021-05-19 18:26:55 +0000

    added tests for campaign

--- Campaign/ethereum/contracts/Campaign.sol
+++ Campaign/ethereum/contracts/Campaign.sol
@@ -1,5 +1,18 @@
 pragma solidity ^0.4.17;
 
+contract CampaignFactory {
+	address[] public deployedCampaigns;
+	
+	function createCampaign(uint minimum) public {
+		address newCampaign = new Campaign(minimum, msg.sender);
+		deployedCampaigns.push(newCampaign);
+	}
+	
+	function getDeployedCampaigns() public view returns (address[]) {
+		return deployedCampaigns;
+	}
+}
+
 contract Campaign {
 	struct Request {
 		string description;
@@ -23,8 +36,9 @@ contract Campaign {
 		_;
 	}
 	
-	constructor(uint minimum) public {
-		manager = msg.sender;
+	
+	constructor(uint minimum, address creator) public {
+		manager = creator;
 		minimumContribution = minimum;
 	}
 	
